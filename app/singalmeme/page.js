1686ecf205defe0254af32d7fbebfdf3d9279ca8
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-10-08 13:01:27 +0500

    generate-image-donne

--- app/singalmeme/page.js
+++ app/singalmeme/page.js
@@ -2,35 +2,51 @@
 
 import { Button } from '@mui/material';
 import Image from 'next/image';
-import React from 'react'
+import { userAgentFromString } from 'next/server';
+import React, { useRef, useState } from 'react'
 
 const SingalMeme = (searchParam) => {
-    const { url, box_count } = searchParam.searchParams
+    const { url, box_count, id } = searchParam.searchParams
     console.log(box_count);
 
-    // generate meme function 
-    const generateMeme = (event) => {
-        event.preventDefault()
-        console.log(box_count);
+    // state for get input value
+    const text1Val = useRef()
+    const text2Val = useRef()
+    const [myMeme, setmyMeme] = useState(null)
 
+    // generate meme function 
+    const generateMeme = async (event) => {
+        event.preventDefault()
+        console.log(text1Val.current.value);
+        console.log(text2Val.current.value);
+        const data = await fetch(`https://api.imgflip.com/caption_image?template_id=${id}&username=haseeburrehmanjs&password=haseeb123&text0=${text1Val.current?.value}&text1=${text2Val.current?.value}`)
+        const respone = await data.json()
+        console.log(respone.data.url);
+        setmyMeme(respone.data.url)
+        text1Val.current.value = ''
+        text2Val.current.value = ''
     }
 
     return (
         <div className='container mx-auto'>
             <h1 className='text-4xl justify-center flex flex-col items-center mt-10 font-bold'>
-                <Image width={300} height={300} src={searchParam.searchParams.url} />
+                <div className='flex justify-center gap-5 mt-5'>
+                    <Image src={searchParam.searchParams.url} width={300} height={300} />
+                    {myMeme && <Image src={myMeme} width={300} height={300} />}
+                </div>
                 <div>
                     <form className='flex flex-col gap-1' onSubmit={generateMeme}>
                         <div className="w-full max-w-sm min-w-[200px]">
-                            <input className="w-[300px] bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow" placeholder="Type here..." />
+                            <input ref={text1Val} className="w-[300px] bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow" placeholder="Type here..." />
                         </div>
                         <div className="w-full max-w-sm min-w-[200px]">
-                            <input className="w-[300px] bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow" placeholder="Type here..." />
+                            <input ref={text2Val} className="w-[300px] bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow" placeholder="Type here..." />
                         </div>
                         <Button type='submit' variant='contained'>submit</Button>
                     </form>
                 </div>
             </h1>
+
         </div>
     )
 }
